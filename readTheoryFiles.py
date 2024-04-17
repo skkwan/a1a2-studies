898289759bf839aa70d272f53fd9a643d8923a0f
skkwan <kystephkwan2@gmail.com>
2024-04-17 10:01:49 +0200

    [fix] fix BR and xsec calculation, improve printout clarity

--- readTheoryFiles.py
+++ readTheoryFiles.py
@@ -19,22 +19,31 @@ for massPointPair in array_mH1_mH2:
 
     # Get the xs and branching ratios we want
     x_H3_gg   = myMassPoint["x_H3_gg"].item()
+    print("Read the values: x_H3_gg: {}".format(x_H3_gg))
     b_H3_H1H1 = myMassPoint["b_H3_H1H1"].item()
+    print("Read the values: b_H3_H1H1: {}".format(b_H3_H1H1))
     b_H3_H1H2 = myMassPoint["b_H3_H1H2"].item()
+    print("Read the values: b_H3_H1H2: {}".format(b_H3_H1H2))
     b_H3_H2H2 = myMassPoint["b_H3_H2H2"].item()
+    print("Read the values: b_H3_H2H2: {}".format(b_H3_H2H2))
     b_H2_H1H1 = myMassPoint["b_H2_H1H1"].item()
+    print("Read the values: b_H2_H1H1: {}".format(b_H2_H1H1))
+
 
     b_H1_bb = myMassPoint["b_H1_bb"].item()
+    print("Read the values: b_H1_bb: {}".format(b_H1_bb))
+
     b_H1_tautau = myMassPoint["b_H1_tautau"].item()
+    print("Read the values: b_H1_tautau: {}".format(b_H1_tautau))
 
-    print("Read the values: x_H3_gg: {}, b_H3_H1H2: {} \n b_H2_H1H1: {} \n b_H1_bb: {} \n b_H1_tautau: {}".format(x_H3_gg, b_H3_H1H2, b_H2_H1H1, b_H1_bb, b_H1_tautau))
 
-    x_4b2tau = b_H3_H1H2 * b_H2_H1H1 * 3*(b_H1_bb * b_H1_bb) * b_H1_tautau
-    x_2b4tau = b_H3_H1H2 * b_H2_H1H1 * 3*(b_H1_tautau * b_H1_tautau) * b_H1_bb
+    b_H3_H1H2_cascade_4b2tau = b_H3_H1H2 * b_H2_H1H1 * 3*(b_H1_bb * b_H1_bb) * b_H1_tautau
+    b_H3_H1H2_cascade_2b4tau = b_H3_H1H2 * b_H2_H1H1 * 3*(b_H1_tautau * b_H1_tautau) * b_H1_bb
 
-    print("xs of 4b2tau: {} \n xs of 2b4tau: {}".format(x_4b2tau, x_2b4tau))
+    print("\t BR(h3->h1h2->cascade->4b2tau), calculated as B(h3 -> h1 h2) * B(h2 -> h1h1) * 3*(h1->bb * h1->bb) * (h1->tautau): {}".format(b_H3_H1H2_cascade_4b2tau))
+    print("\t BR(h3->h1h2->cascade->2b4tau), calculated as B(h3 -> h1 h2) * B(h2 -> h1h1) * 3*(h1->tautau * h1->tautau) * (h1->bb): {}".format(b_H3_H1H2_cascade_2b4tau))
 
-    print("xs of H3 -> H1H2 -> 3H1 {}".format(x_H3_gg * b_H3_H1H2 * b_H2_H1H1))
+    print("xs of ggH H3 -> H1H2 -> cascade -> 4b2tau: {}".format(x_H3_gg * b_H3_H1H2_cascade_4b2tau))
 
-    print("b_H3_H1H1: {}, b_H3_H1H2 {}, b_H3_H2H2 {}".format(b_H3_H1H1, b_H3_H1H2, b_H3_H2H2))
+    print("xs of ggH H3 -> H1H2 -> cascade -> 2b4tau: {}".format(x_H3_gg * b_H3_H1H2_cascade_2b4tau))
 
