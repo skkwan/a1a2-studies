21863f8756bc97e5e8e056f4c4e8982bf5440932
skkwan <kystephkwan2@gmail.com>
2024-04-17 10:51:25 +0200

    add branching fraction and xsec figures

--- readTheoryFiles.py
+++ readTheoryFiles.py
@@ -1,6 +1,10 @@
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 df = pd.read_table("/Users/stephaniekwan/Dropbox/Princeton_G6/hToA1A2/fromTheorists/BP1.tsv",index_col=0)
+
+# plt.rcParams.update({
+#     "text.usetex": True
+# })
 
 # for col in df.columns:
 #     print(col)
@@ -9,7 +13,31 @@ df = pd.read_table("/Users/stephaniekwan/Dropbox/Princeton_G6/hToA1A2/fromTheori
 
 ## Asymmetric case
 print("Asymmetric case...")
-array_mH1_mH2 = [(15, 100)]
+# 9 points with m_1 = 15 GeV, 7 points with m_1 = 20 GeV, 4 points with m_1 = 30 GeV
+array_mH1_mH2 = [  
+    (15, 40),
+    (15, 50),
+    (15, 60),
+    (15, 70),
+    (15, 80),
+    (15, 90),
+    (15, 100),
+    (15, 110),
+    (20, 40), 
+    (20, 50),
+    (20, 60),
+    (20, 70),
+    (20, 80),
+    (20, 90),
+    (20, 100),
+    (30, 60),
+    (30, 70),
+    (30, 80),
+    (30, 90),
+]
+
+br_values = []
+xs_ggh_values = []
 
 for massPointPair in array_mH1_mH2:
     (value_mH1, value_mH2) = massPointPair
@@ -47,3 +75,45 @@ for massPointPair in array_mH1_mH2:
 
     print("xs of ggH H3 -> H1H2 -> cascade -> 2b4tau: {}".format(x_H3_gg * b_H3_H1H2_cascade_2b4tau))
 
+    br_values.append(b_H3_H1H2_cascade_4b2tau)
+    xs_ggh_values.append(x_H3_gg * b_H3_H1H2_cascade_4b2tau)
+
+print(br_values)
+
+## Branching fraction 
+# First 9 values are m_1 = 15 GeV
+fig, ax = plt.subplots()
+
+# Add grid
+plt.grid(axis='both', color='0.95')
+ax.plot([item[1] for item in array_mH1_mH2[0:8]], br_values[0:8], 'o-', label='$m_{1} = 15$ GeV')
+ax.plot([item[1] for item in array_mH1_mH2[9:15]], br_values[9:15], 'o-', label='$m_{1} = 20$ GeV')
+ax.plot([item[1] for item in array_mH1_mH2[16:19]], br_values[16:19], 'o-', label='$m_{1} = 20$ GeV')
+
+ax.set_xlabel("Mass of $m_{2}$ (GeV)")
+ax.set_ylabel("Branching fraction B(h $\\rightarrow a_1 a_2 \\rightarrow 3a_1 \\rightarrow 4b2\\tau$)")
+
+plt.title("Branching fraction of $h_{125}$ cascade decay to $4b2\\tau$")
+
+plt.legend()
+plt.tight_layout()
+plt.savefig("example_br.pdf")
+
+## Branching fraction 
+# First 9 values are m_1 = 15 GeV
+fig, ax = plt.subplots()
+
+# Add grid
+plt.grid(axis='both', color='0.95')
+ax.plot([item[1] for item in array_mH1_mH2[0:8]], xs_ggh_values[0:8], 'o-', label='$m_{1} = 15$ GeV')
+ax.plot([item[1] for item in array_mH1_mH2[9:15]], xs_ggh_values[9:15], 'o-', label='$m_{1} = 20$ GeV')
+ax.plot([item[1] for item in array_mH1_mH2[16:19]], xs_ggh_values[16:19], 'o-', label='$m_{1} = 30$ GeV')
+
+ax.set_xlabel("Mass of $m_{2}$ (GeV)")
+ax.set_ylabel("$\sigma$ of ggH $h_{125}$ cascade decay to $4b2\\tau$ (pb)")
+
+plt.title("$\sigma$ of $h_{125}$ cascade decay to $4b2\\tau$, assuming SM ggH production")
+
+plt.legend()
+plt.tight_layout()
+plt.savefig("example_ggh_xsec.pdf")
